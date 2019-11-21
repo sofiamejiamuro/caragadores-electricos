ae99d34abc8632e18b114fbcc0c47f56b4914a28
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-21 12:26:39 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Navigation from './components/Navigation';
+import Map from './views/Map';
+
+function App() {
+  return (
+    <Router>
+      {/* <Navigation/> */}
+      <div className="container p-1">
+        {/* <Route path="/" exact component={Login}/> */}
+        <Route path="/Map" component={Map}/>
+        {/* <Route path="/create" component={CreateNote}/>
+        <Route path="/user" component={CreateUser}/> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
