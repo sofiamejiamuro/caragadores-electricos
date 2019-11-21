8bf38f842bd46ab3465e7c31f5475a9caba95fdc
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-21 15:25:41 -0600

    vista mapa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Navigation from './components/Navigation';
-import Map from './views/Map';
+import Mapa from './views/Mapa';
 
 function App() {
   return (
@@ -11,12 +11,12 @@ function App() {
       {/* <Navigation/> */}
       <div className="container p-1">
         {/* <Route path="/" exact component={Login}/> */}
-        <Route path="/Map" component={Map}/>
+        <Route path="/Mapa" component={Mapa}/>
         {/* <Route path="/create" component={CreateNote}/>
         <Route path="/user" component={CreateUser}/> */}
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
