edb81a5d4fa929cf2842b26580f5414b69d84b78
carolinaab <tau_karo@hotmail.com>
2019-11-21 17:19:18 -0600

    se agrega toggle

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Mapa from './views/Mapa';
 import Navigation from './components/Navigation';
+import TypePlugs from './views/TypePlugs'
 
 
 function App() {
   return (
     <Router>
-      {<Navigation/>}
+      {<Navigation />}
       <div className="container p-1">
         {/* <Route path="/" exact component={Login}/> */}
-        <Route path="/Mapa" component={Mapa}/>
-        {/* <Route path="/create" component={CreateNote}/>
-        <Route path="/user" component={CreateUser}/> */}
+        <Route path="/Mapa" component={Mapa} />
+        <Route path="/cargadores" component={TypePlugs} />
+        {/* <Route path="/user" component={CreateUser} /> */}
       </div>
     </Router>
   )
