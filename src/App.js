5cf174530cdae65f0d404728b82b62d447f02b41
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-22 00:07:53 -0600

    actualizando rutas

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navigation from './components/Navigation';
 import Alarma from './views/Alarma';
 import ServicioVoz from './views/Servicio-voz';
-
 import Mapa from './views/Mapa';
 import Navigation from './components/Navigation';
 import Historial from './views/Historial';
@@ -23,9 +21,6 @@ function App() {
       <div className="container p-1">
         {/* <Route path="/" exact component={Login}/> */}
 
-        <Route path="/Mapa" component={Mapa}/>
-        {/* <Route path="/create" component={CreateNote}/>
-        <Route path="/user" component={CreateUser}/> */}
         <Route path="/Alarma" component={Alarma}/>
         <Route path="/Servicio-voz" component={ServicioVoz}/>
         <Route path="/Historial" component={Historial}/>
@@ -34,7 +29,6 @@ function App() {
         <Route path="/Historial3" component={Historial3}/>
         <Route path="/mapa" component={Mapa} />
         <Route path="/cargadores" component={TypePlugs} />
-        {/* <Route path="/user" component={CreateUser} /> */}
 
       </div>
     </Router>
