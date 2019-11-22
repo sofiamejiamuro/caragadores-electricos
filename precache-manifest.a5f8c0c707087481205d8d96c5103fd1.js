135f175068d12c3316e3838328c29b86117963d3
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-11-22 15:32:46 -0600

    Updates

---
+++ precache-manifest.a5f8c0c707087481205d8d96c5103fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711f0640c946a8f3baa79eeffceb1d1e",
+    "url": "/caragadores-electricos/index.html"
+  },
+  {
+    "revision": "0c10eda796ec12692449",
+    "url": "/caragadores-electricos/static/css/2.8b70035b.chunk.css"
+  },
+  {
+    "revision": "8cd22fe471ff3d93439d",
+    "url": "/caragadores-electricos/static/css/main.950057d3.chunk.css"
+  },
+  {
+    "revision": "0c10eda796ec12692449",
+    "url": "/caragadores-electricos/static/js/2.2ef082e3.chunk.js"
+  },
+  {
+    "revision": "8cd22fe471ff3d93439d",
+    "url": "/caragadores-electricos/static/js/main.65b2701b.chunk.js"
+  },
+  {
+    "revision": "5a14523ccfc0d45ead7a",
+    "url": "/caragadores-electricos/static/js/runtime-main.02d67195.js"
+  }
+]);
